682712c461a32795530d7f513142e6d5c887561a
Adam Antok <adam@antok.me>
2017-12-24 20:10:04 +0100

    Cleaned up the whitespaces

--- backup.sh
+++ backup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# change to your own zpool
+
 SOURCE_ZPOOL="zeefes"
 
 EXPORT=false
@@ -16,9 +18,9 @@ done
 
 if [ -z "$SOURCE_DATASET" ]
 then
-	echo "no dataset defined."
-	echo "Usage: backup -d name_of_dataset"
-	exit
+  echo "no dataset defined."
+  echo "Usage: backup -d name_of_dataset"
+  exit
 fi
 
 zpool import -a
@@ -34,10 +36,10 @@ BACKUP_ZPOOL=`zpool list | grep 'backup' | cut -f1 -d' '`
 
 if [ -z "$BACKUP_ZPOOL" ]
 then
-	echo "backup zpool not found --> EXIT"
-	exit
+  echo "backup zpool not found --> EXIT"
+  exit
 else
-	echo "found backup zpool with the name: $BACKUP_ZPOOL"
+  echo "found backup zpool with the name: $BACKUP_ZPOOL"
 fi
 
 LAST_SNAPSHOT=`zfs list -t snapshot -o name -s creation | grep ^$BACKUP_ZPOOL/$SOURCE_DATASET | tail -n 1 | cut -f2 -d'@'`
@@ -46,27 +48,27 @@ echo "the last snapshot on the backup device: $LAST_SNAPSHOT"
 
 if (zfs list -t snapshot -o name -s creation | grep -q "$SOURCE_ZPOOL/$SOURCE_DATASET@$LAST_SNAPSHOT")
 then
-	LAST_LIVE_SNAPSHOT=`zfs list -t snapshot -o name -s creation | grep ^$SOURCE_ZPOOL/$SOURCE_DATASET | tail -n 1 | cut -f2 -d'@'`
-	echo "the last snapshot on the live device: $LAST_LIVE_SNAPSHOT"
-	if [ "$LAST_LIVE_SNAPSHOT" == "$LAST_SNAPSHOT" ]; then
-		echo "backup is up to date, no need to send stream"
-	else
-		echo "new snapshot is found, sending incremental backup"
-		zfs send -vi $LAST_SNAPSHOT `zfs list -t snapshot -o name -s creation | grep ^$SOURCE_ZPOOL/$SOURCE_DATASET | tail -n 1` | zfs receive -F $BACKUP_ZPOOL/$SOURCE_DATASET
-	fi
-	
+  LAST_LIVE_SNAPSHOT=`zfs list -t snapshot -o name -s creation | grep ^$SOURCE_ZPOOL/$SOURCE_DATASET | tail -n 1 | cut -f2 -d'@'`
+  echo "the last snapshot on the live device: $LAST_LIVE_SNAPSHOT"
+  if [ "$LAST_LIVE_SNAPSHOT" == "$LAST_SNAPSHOT" ]; then
+    echo "backup is up to date, no need to send stream"
+  else
+    echo "new snapshot is found, sending incremental backup"
+    zfs send -vi $LAST_SNAPSHOT `zfs list -t snapshot -o name -s creation | grep ^$SOURCE_ZPOOL/$SOURCE_DATASET | tail -n 1` | zfs receive -F $BACKUP_ZPOOL/$SOURCE_DATASET
+  fi
+  
 
 else
-	echo "last snapshot not found, sending full stream"
-	zfs destroy -v $BACKUP_ZPOOL/$SOURCE_DATASET@%
-	zfs send -v `zfs list -t snapshot -o name -s creation | grep ^$SOURCE_ZPOOL/$SOURCE_DATASET | tail -n 1` | zfs receive -F $BACKUP_ZPOOL/$SOURCE_DATASET
+  echo "last snapshot not found, sending full stream"
+  zfs destroy -v $BACKUP_ZPOOL/$SOURCE_DATASET@%
+  zfs send -v `zfs list -t snapshot -o name -s creation | grep ^$SOURCE_ZPOOL/$SOURCE_DATASET | tail -n 1` | zfs receive -F $BACKUP_ZPOOL/$SOURCE_DATASET
 fi
 
 if [ "$EXPORT" = true ]
 then
-	echo "exporting zpool: $BACKUP_ZPOOL"
-	zpool export $BACKUP_ZPOOL
+  echo "exporting zpool: $BACKUP_ZPOOL"
+  zpool export $BACKUP_ZPOOL
 else
-	echo "$BACKUP_ZPOOL not exported!"
+  echo "$BACKUP_ZPOOL not exported!"
 fi
-	
+
